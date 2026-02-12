885009c8710249f10c98df9231d710b50ef0ea9b
boadijoseph7177 <boadi_j@live.bluefieldstate.edu>
2026-02-11 22:11:43 -0500

    mock gra api setup started

--- backend-go/main.go
+++ backend-go/main.go
@@ -1,22 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Invoice struct {
+	InvoiceID string  `json:"invoice_id"`
+	Amount    float64 `json:"amount"`
+	VATRate   float64 `json:"vat_rate"`
+	Customer  string  `json:"customer"`
+}
+
+func validateInvoice(inv Invoice) error {
+	if inv.InvoiceID == "" {
+		return fmt.Errorf("Invoice ID is required")
+	}
+	if inv.Amount <= 0 {
+		return fmt.Errorf("Amount must be greater than zero")
+	}
+
+	return nil
+}
+
 func main() {
 
-	const port = ":8080"
+	http.HandleFunc("/invoice", createInvoice)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Backend is LIVE - Ready for E-VAT 2026")
-	})
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 
-	fmt.Printf("Server starting on http://localhost%s...\n", port)
+}
 
-	err := http.ListenAndServe(port, nil)
-	if err != nil {
-		fmt.Printf("Error starting server: %\n", err)
+func createInvoice(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
+
+	var invoice Invoice
+
+	err := json.NewDecoder(r.Body).Decode(&invoice)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	response, _ := json.Marshal(invoice)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(response)
 }
